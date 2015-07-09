20030502be50a9e10e1792b99f5bcb4cd068dd39
Vasiliy <vasiliy.denezhkin@gmail.com>
2015-07-09 21:14:37 +0300

    no message

--- Cubiki/Release/fileloader.js
+++ Cubiki/Release/fileloader.js
@@ -26,8 +26,8 @@ Module.expectedDataFileDownloads++;
     var REMOTE_PACKAGE_NAME = typeof Module['locateFile'] === 'function' ?
                               Module['locateFile'](REMOTE_PACKAGE_BASE) :
                               ((Module['filePackagePrefixURL'] || '') + REMOTE_PACKAGE_BASE);
-    var REMOTE_PACKAGE_SIZE = 2855189;
-    var PACKAGE_UUID = '47ebcb87-5c15-4a1d-add4-8835886e52fb';
+    var REMOTE_PACKAGE_SIZE = 2857148;
+    var PACKAGE_UUID = '39d71779-2684-4bcc-82a8-052148479738';
   
     function fetchRemotePackage(packageName, packageSize, callback, errback) {
       var xhr = new XMLHttpRequest();
@@ -125,11 +125,11 @@ Module['FS_createPath']('/', 'Resources', true, true);
         this.requests[this.name] = null;
       },
     };
-      new DataRequest(0, 176684, 0, 0).open('GET', '/mainData');
-    new DataRequest(176684, 176821, 0, 0).open('GET', '/methods_pointedto_by_uievents.xml');
-    new DataRequest(176821, 779677, 0, 0).open('GET', '/sharedassets0.assets');
-    new DataRequest(779677, 2354713, 0, 0).open('GET', '/Resources/unity_default_resources');
-    new DataRequest(2354713, 2855189, 0, 0).open('GET', '/Resources/unity_builtin_extra');
+      new DataRequest(0, 178500, 0, 0).open('GET', '/mainData');
+    new DataRequest(178500, 178780, 0, 0).open('GET', '/methods_pointedto_by_uievents.xml');
+    new DataRequest(178780, 781636, 0, 0).open('GET', '/sharedassets0.assets');
+    new DataRequest(781636, 2356672, 0, 0).open('GET', '/Resources/unity_default_resources');
+    new DataRequest(2356672, 2857148, 0, 0).open('GET', '/Resources/unity_builtin_extra');
 
     function processPackageData(arrayBuffer) {
       Module.finishedDataFileDownloads++;
